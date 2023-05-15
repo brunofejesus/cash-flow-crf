cab63ddef1b2cf419585558fe15dbec196aeda07
bferreiraj <brunofejesusdev@outlook.com>
2023-05-15 09:04:49 -0300

    Initial commit project

--- Crf/Crf.Infrastructure/Persistence/Configurations/ActivityConfiguration.cs
+++ Crf/Crf.Infrastructure/Persistence/Configurations/ActivityConfiguration.cs
@@ -4,37 +4,38 @@ using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace Crf.Infrastructure.Persistence.Configurations
 {
-  public class ActivityConfiguration : IEntityTypeConfiguration<Activity>
-  {
-    public void Configure(EntityTypeBuilder<Activity> builder)
-    {
-      builder.ToTable("activity");
+	public class ActivityConfiguration : IEntityTypeConfiguration<Activity>
+	{
+		public void Configure(EntityTypeBuilder<Activity> builder)
+		{
+			builder.ToTable("activity");
 
-      builder.HasKey(x => x.Id);
+			builder.HasKey(x => x.Id);
 
-      builder.Property(x => x.Id)
-        .HasColumnName("activity_id")
-        .IsRequired();
+			builder.Property(x => x.Id)
+				.HasColumnName("activity_id")
+				.IsRequired();
 
-      builder.Property(x => x.TypeActivity)
-        .HasColumnName("type_activity")
-        .IsRequired();
+			builder.Property(x => x.TypeActivity)
+				.HasColumnName("type_activity")
+				.HasColumnType("e_type_activity")
+				.IsRequired();
 
-      builder.Property(x => x.AccountId)
-        .HasColumnName("activity_account_id")
-        .IsRequired();
+			builder.Property(x => x.AccountId)
+				.HasColumnName("activity_account_id")
+				.IsRequired();
 
-      builder.Property(x => x.Created)
-        .HasColumnName("activity_created")
-        .IsRequired();
+			builder.Property(x => x.Created)
+				.HasColumnName("activity_created")
+				.IsRequired();
 
-      builder.Property(x => x.Description)
-        .HasColumnName("activity_description")
-        .IsRequired();
+			builder.Property(x => x.Description)
+				.HasColumnName("activity_description")
+				.IsRequired();
 
-      builder.HasOne(x => x.Account)
-        .WithMany(x => x.Activities)
-        .HasForeignKey(x => x.AccountId);
-    }
-  }
+			builder.HasOne(x => x.Account)
+				.WithMany(x => x.Activities)
+				.HasForeignKey(x => x.AccountId);
+		}
+	}
 }
